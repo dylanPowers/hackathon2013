c3f3c0c7f018f34855f30ef04f8583caf262e77d
Dylan Powers <dylan.powers@email.wsu.edu>
2013-10-19 11:52:57 -0700

    Simple web server implementation

--- hackathon2013/server/file_handler.dart
+++ hackathon2013/server/file_handler.dart
@@ -2,10 +2,14 @@ part of server;
 
 
 class FileHandler {
+  String _basePath;
+  
+  FileHandler(Directory dir) {
+    _basePath = "${dir.path}../web/";
+  }
   
   void onRequest(HttpRequest request) {
     
     
-  }
-  
+  }  
 }
