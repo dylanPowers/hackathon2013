55280ed9adcdc10140e97a5bf81f6dd7591ccadc
Dylan Powers <dylan.powers@email.wsu.edu>
2013-10-19 12:12:47 -0700

    File handler for dishing out files to browsers

--- hackathon2013/server/file_handler.dart
+++ hackathon2013/server/file_handler.dart
@@ -2,14 +2,35 @@ part of server;
 
 
 class FileHandler {
-  String _basePath;
+  final String _basePath;
   
-  FileHandler(Directory dir) {
-    _basePath = "${dir.path}../web/";
+  FileHandler(Directory dir) : 
+      _basePath = new Directory("${dir.path}../web/").absolute.toString();
+  
+  void _send404(HttpResponse response) {
+    response.statusCode = HttpStatus.NOT_FOUND;
+    response.close();
   }
   
   void onRequest(HttpRequest request) {
+    String path = request.uri.path;
+    if (request.uri.path == '/') {
+      path = '/hackathon2013.html';
+    }
     
-    
+    final File file = new File('${_basePath}${path}');
+    file.exists().then((bool found) {
+      if (found) {
+        if (!file.absolute.path.startsWith(_basePath)) {
+          _send404(request.response);
+        } else {
+          file.openRead().pipe(request.response).catchError((error) {
+            print("Error reading the file ${file.absolute.path}: $error");
+          });
+        }
+      } else {
+        _send404(request.response);
+      }
+    });    
   }  
 }
