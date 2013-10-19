c82b41d3a89deed6979e1356dd19aa74d1e2f7ae
eschimpf <evan.schimpf@email.wsu.edu>
2013-10-19 13:38:18 -0700

    Renamed to proper conventions

---
+++ hackathon2013/web/chat_controller.dart
@@ -0,0 +1,11 @@
+import 'chat_window.dart';
+import 'input_window.dart';
+
+
+
+class ChatController {
+  ChatWindow chatWindow;
+  InputWindow inputWindow;
+  
+  
+}
--- hackathon2013/web/hackathon2013.dart
+++ hackathon2013/web/hackathon2013.dart
@@ -11,29 +11,10 @@ const String WEB_ADDRESS = "localhost";
 
 void main() {
   
-  chatWindow1 = query("#chatWindowId1");
-  inputWindow = query("#inputWindowId");
-  sendButton = query("#sendButtonId");
-  
-  //try {
-    //webSocket = new WebSocket(WEB_ADDRESS);
-  //} catch(e) {
-    
-  //}
-    
-  sendButton.onClick.listen(send);
-  
   window.onSubmit.listen(enter);
-}
-
-void send(MouseEvent e) {
-  if(inputWindow.value != "") {
-    chatWindow1.value = chatWindow1.value + inputWindow.value + "\n";
-    inputWindow.value = "";
-  }
+  
 }
 
 void enter(Event e) {
   e.preventDefault();
-  sendButton.click();
 }
