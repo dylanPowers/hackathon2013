3c81ee951f46092e77dfbc0f69237f26cd14ae74
Dylan Powers <dylan.powers@email.wsu.edu>
2013-10-19 19:15:39 -0700

    Cool new styling

--- hackathon2013/web/message_box.dart
+++ hackathon2013/web/message_box.dart
@@ -3,5 +3,7 @@ import 'package:polymer/polymer.dart';
 @CustomTag('message-box')
 class MessageBox extends PolymerElement {
   @observable String boxText = 'Empty Message Box';
-  @observable String cssClassProps = ".right";
+  @observable String cssClassProps = "right";
+  
+  get applyAuthorStyles => true;
 }
