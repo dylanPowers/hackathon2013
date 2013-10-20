2fca6a023e0719801014968f462712ffa2acb7d8
Dylan Powers <dylan.powers@email.wsu.edu>
2013-10-19 18:44:47 -0700

    CSS beginnings

--- hackathon2013/web/message_box.dart
+++ hackathon2013/web/message_box.dart
@@ -3,4 +3,5 @@ import 'package:polymer/polymer.dart';
 @CustomTag('message-box')
 class MessageBox extends PolymerElement {
   @observable String boxText = 'Empty Message Box';
+  @observable String cssClassProps = ".right";
 }
